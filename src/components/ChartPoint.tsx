5e6c7ce5e7e1b9fa15c3ad77a4ee12e04889f26a
anlakngs <anilakgunes1@gmail.com>
2021-12-12 02:58:20 +0300

    fix css bug

--- src/components/ChartPoint.tsx
+++ src/components/ChartPoint.tsx
@@ -2,7 +2,10 @@ import { useState, useRef, useEffect, useContext } from 'react';
 import classes from './ChartPoint.module.css';
 import * as types from '../shared/types';
 import { AppContext } from '../shared/state/AppState';
-import { converterTo100, converterTo400 } from '../shared/utilities';
+import {
+  converterFrom385to100,
+  converterFrom100to385,
+} from '../shared/utilities';
 
 interface ChartPointProps {
   itemData: types.Item;
@@ -11,52 +14,52 @@ interface ChartPointProps {
 
 const ChartPoint: React.FC<ChartPointProps> = ({ itemData, id }) => {
   const { updateItem, itemList } = useContext(AppContext);
+  const [isPointOutlined, setIsPointOutlined] = useState<boolean>(false);
   const [isDragging, setIsDragging] = useState<Boolean>(false);
   const mainRef = useRef<HTMLDivElement | null>(null);
   const pointRef = useRef<HTMLDivElement | null>(null);
-  const outlineRef = useRef<HTMLDivElement | null>(null);
 
   // updating css for points
   useEffect(() => {
     if (mainRef.current) {
-      mainRef.current.style.transform = `translate(${converterTo400(
+      mainRef.current.style.transform = `translate(${converterFrom100to385(
         itemData.x
-      )}px, ${converterTo400(itemData.y)}px)`;
+      )}px, ${converterFrom100to385(itemData.y)}px)`;
     }
   }, [itemList, itemData.x, itemData.y]);
 
-  // Because of a css bug the range 390 not 400 for chart
+  // to keep the point inside chart
   const squareConstraints = (data: number) => {
-    if (data > 400) return converterTo100(390);
-    if (data < -10) return 0;
-    return converterTo100(data);
+    if (data > 385) return 385;
+    if (data < 0) return 0;
+    return data;
   };
 
   const mouseMoveHandler = (event: React.MouseEvent) => {
     if (isDragging) {
       event.preventDefault();
       updateItem(id, {
-        x: squareConstraints(converterTo400(itemData.x) + event.movementX),
-        y: squareConstraints(converterTo400(itemData.y) + event.movementY),
+        x: converterFrom385to100(
+          squareConstraints(converterFrom100to385(itemData.x) + event.movementX)
+        ),
+        y: converterFrom385to100(
+          squareConstraints(converterFrom100to385(itemData.y) + event.movementY)
+        ),
         label: itemData.label,
       });
     }
   };
 
   const mouseDownHandler = (event: React.MouseEvent) => {
-    setIsDragging(true);
-    if (outlineRef.current) {
-      outlineRef.current.style.opacity = '1';
-    }
     event.preventDefault();
+    setIsDragging(true);
+    setIsPointOutlined(true);
   };
 
   const mouseUpHandler = (event: React.MouseEvent) => {
-    setIsDragging(false);
-    if (outlineRef.current) {
-      outlineRef.current.style.opacity = '0';
-    }
     event.preventDefault();
+    setIsDragging(false);
+    setIsPointOutlined(false);
   };
 
   return (
@@ -65,11 +68,15 @@ const ChartPoint: React.FC<ChartPointProps> = ({ itemData, id }) => {
       onMouseMove={mouseMoveHandler}
       onMouseDown={mouseDownHandler}
       onMouseUp={mouseUpHandler}
+      onMouseLeave={mouseUpHandler}
       className={classes.main}
     >
       <p className={classes.label}>{itemData.label}</p>
       <div className={classes.point} ref={pointRef}>
-        <div className={classes.outline} ref={outlineRef} />
+        <div
+          className={classes.outline}
+          style={{ opacity: isPointOutlined ? 1 : 0 }}
+        />
       </div>
     </div>
   );
--- src/shared/utilities.tsx
+++ src/shared/utilities.tsx
@@ -1,14 +1,14 @@
-
 // for unique item id;
 export const randomId = () => {
   return Math.random().toString(36).substring(2, 5);
 };
 
-// to keep the range 0 100 instead of 400 in inputs
-export const converterTo400 = (num: number) => {
-  return num * 4 - 10;
+// to keep the range 385 to 100 for inputs
+export const converterFrom385to100 = (num: number) => {
+  return +((num / 385) * 100).toFixed(2);
 };
 
-export const converterTo100 = (num: number) => {
-  return +((num + 10) / 4).toFixed(2);
+// to keep the range 385 to 100 for CSS transition
+export const converterFrom100to385 = (num: number) => {
+  return +((num / 100) * 385).toFixed(2);
 };
